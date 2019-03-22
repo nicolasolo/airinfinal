6ff5818435c4da4d9b987874d7d8eb7978936962
Nicola Solo <solo.nicola@live.com>
2019-03-22 03:33:55 +0200

    aa

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
+const items = require('.routes/api/items'),
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+
+const app = express();
+
+
+app.use(bodyParser.json());
+
+app.use('/api/items', items);
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', build, index.html));
+
+    });
+}
+
+mongoose
+    .connect('mongodb+srv://nicola:samsung330@cluster0-8t26p.mongodb.net/prova?retryWrites=true')
+    .then(() => console.log('mongodb connected you fool'))
+    .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server Started on port ${port}`));
