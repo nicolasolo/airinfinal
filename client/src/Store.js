96758bf9d3eb2ea6bd103783ca5e8057563e6338
Nicola Solo <solo.nicola@live.com>
2019-03-25 09:27:19 +0200

    mmm

--- client/src/Store.js
+++ client/src/Store.js
@@ -9,8 +9,7 @@ const store = createStore(
     initialState,
     compose(
         applyMiddleware(...middleWare),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
     )
-
 );
 export default store;
